5474ed4b0fa2ed47036653cd63975d6d3c52b4a6
gabriel140282 <gkaakmor@myuax.com>
2024-12-01 00:10:49 +0100

    V0.5

--- Tarea/tarea.cpp
+++ Tarea/tarea.cpp
@@ -1,1 +1,25 @@
 #include "tarea.h"
+#include <iostream>
+
+Tarea::Tarea(const std::string& id, const std::string& descripcion, int duracion)
+    : id(id), desc(descripcion), duracion(duracion) {}
+
+std::string Tarea::obtener_id() const {
+    return id;
+}
+
+std::string Tarea::obtener_descripcion() const {
+    return desc;
+}
+
+std::string Tarea::toString() const {
+    return "Descripcion de la tarea: " + desc + ", Tiempo estimado: " + std::to_string(duracion) + " minutos";
+}
+
+int Tarea::obtener_duracion() const {
+    return duracion;
+}
+
+int Tarea::obtener_duraciontotal() const {
+    return duracion;
+}
--- Tarea/tarea.h
+++ Tarea/tarea.h
@@ -1,4 +1,22 @@
 #ifndef TAREA_H
 #define TAREA_H
 
-#endif //TAREA_H
+#include <string>
+
+class Tarea {
+private:
+    std::string id;
+    std::string desc;
+    int duracion;
+
+public:
+    Tarea(const std::string& id, const std::string& descripcion, int duracion);
+    std::string obtener_id() const;
+    std::string obtener_descripcion() const;
+    int obtener_duracion() const;
+    std::string toString() const;
+
+    int obtener_duraciontotal() const;
+};
+
+#endif // TAREA_H
