fc2cbb174947ed6707ae08b79752d0c4ce10608a
gabriel140282 <gkaakmor@myuax.com>
2024-12-01 00:16:32 +0100

    V1.0

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
-#include "Tarea/tarea.h"
+#include "./Launcher/launcher.h"
+#include "./Crud/crud.h"
 
 int main() {
-
+    Launcher launcher;
+    Crud crud;
+    launcher.ejecutar();
+    crud.iniciarCrud();
     return 0;
 }
